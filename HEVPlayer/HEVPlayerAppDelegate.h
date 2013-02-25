06e997db873aaa5b8ed98030a87f8318b759d1a3
shengbinmeng <shengbinmeng@gmail.com>
2013-02-25 10:04:02 +0800

    Initial Commit

---
+++ HEVPlayer/HEVPlayerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HEVPlayerAppDelegate.h
+//  HEVPlayer
+//
+//  Created by Shengbin Meng on 13-2-25.
+//  Copyright (c) 2013年 Peking University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HEVPlayerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HEVPlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HEVPlayer
+//
+//  Created by Shengbin Meng on 13-2-25.
+//  Copyright (c) 2013年 Peking University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HEVPlayerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HEVPlayerAppDelegate class]));
+    }
+}
