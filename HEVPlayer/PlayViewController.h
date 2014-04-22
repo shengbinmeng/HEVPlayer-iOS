e54bc5aad2e93c73b62ea7ee1edf9b94e82b9277
Shengbin Meng <shengbinmeng@gmail.com>
2014-04-22 11:11:06 +0800

    add the play view UI

--- HEVPlayer/PlayViewController.h
+++ HEVPlayer/PlayViewController.h
@@ -12,8 +12,21 @@
 @interface PlayViewController : UIViewController
 
 @property (nonatomic, retain) IBOutlet UILabel *infoLabel;
+@property (nonatomic, retain) IBOutlet UILabel *playingTimeLabel;
+@property (nonatomic, retain) IBOutlet UILabel *leftTimeLabel;
 @property (nonatomic, retain) IBOutlet UIButton *doneButton;
+@property (nonatomic, retain) IBOutlet UIButton *pauseButton;
+@property (nonatomic, retain) IBOutlet UISlider *progressIndicator;
+
 @property (nonatomic, retain) MoviePlayer *player;
+
 - (IBAction)doneButtonPressed:(id)sender;
+-(IBAction)pauseButtonPressed:(id)sender;
+
+- (void) showOverlayViews;
+- (void) hideOverlayViews;
+
+- (void) monitorPlaybackTime;
+- (IBAction) onSliderTouchDragExit: (UISlider*)sender;
 
 @end
--- HEVPlayer/PlayViewController.m
+++ HEVPlayer/PlayViewController.m
@@ -15,6 +15,10 @@ GLRenderer *gRenderer;
 
 {
     bool isPlaying;
+    float lastFrameTime;
+    BOOL overlayHidden;
+    int flag;
+
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -31,6 +35,33 @@ GLRenderer *gRenderer;
     if (!isPlaying) {
         return;
     }
+    
+    double time = [self.player getMovieTimeInSeconds];
+    double duration = [self.player getMovieDurationInSeconds];
+    if (duration - time < 2) {
+        [self doneButtonPressed:nil];
+    }
+    if (flag == -1) {
+        // not open movie, so return
+        return;
+    }
+    if (flag > 0) {
+        // just seek, wait a while (3 seconds) for the correct time value
+        flag --;
+    } else {
+        self.progressIndicator.value = time / (double)duration;
+    }
+    
+    int hour, minute, second;
+    hour = (int64_t) time / 3600;
+    minute = ((int64_t)time % 3600) / 60;
+    second = ((int64_t)time % 3600) % 60;
+    [self.playingTimeLabel setText:[NSString stringWithFormat:@"%2.2d:%2.2d:%2.2d",hour, minute, second]];
+    
+    hour = (int64_t)(duration - time) / 3600;
+    minute = ((int64_t)(duration - time) % 3600) / 60;
+    second = ((int64_t)(duration - time) % 3600) % 60;
+    [self.leftTimeLabel setText:[NSString stringWithFormat:@"%2.2d:%2.2d:%2.2d",hour, minute, second]];
 
     [self.infoLabel setText:self.player.infoString];
     [self performSelector:@selector(monitorPlaybackTime) withObject:nil afterDelay:1.0];
@@ -108,6 +139,65 @@ GLRenderer *gRenderer;
     [self.player close];
     [[UIApplication sharedApplication] setIdleTimerDisabled:NO];
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+
+-(IBAction)pauseButtonPressed:(id)sender
+{
+    if([self.player movieIsPlaying] == YES) {
+        [self.player pause];
+        [sender setTitle:@"Play" forState:UIControlStateNormal];
+    } else {
+        [self.player go];
+        [sender setTitle:@"Pause" forState:UIControlStateNormal];
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    if ([touch tapCount] < 2) return;
+    if ([touch view] == [self view]) {
+        if(overlayHidden == YES) {
+            [self showOverlayViews];
+        } else {
+            [self hideOverlayViews];
+        }
+    }
+}
+
+- (void) hideOverlayViews
+{
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
+    [self.infoLabel setHidden:YES];
+    [self.doneButton setHidden:YES];
+    [self.pauseButton setHidden:YES];
+    [self.progressIndicator setHidden:YES];
+    [self.playingTimeLabel setHidden:YES];
+    [self.leftTimeLabel setHidden:YES];
+    overlayHidden = YES;
+}
+
+- (void) showOverlayViews
+{
+    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationFade];
+    [self.infoLabel setHidden:NO];
+    [self.doneButton setHidden:NO];
+    [self.pauseButton setHidden:NO];
+    [self.progressIndicator setHidden:NO];
+    [self.playingTimeLabel setHidden:NO];
+    [self.leftTimeLabel setHidden:NO];
+    overlayHidden = NO;
+}
+
+- (IBAction) onSliderTouchDragExit: (UISlider*)sender
+{
+    int64_t time = [self.player getMovieDurationInSeconds] * self.progressIndicator.value;
+    if ([self.player getMovieDurationInSeconds] - time < 10) {
+        time = [self.player getMovieDurationInSeconds] - 10;
+    }
+    [self.player seekTo:time];
+    flag = 2;
 }
 
 @end
