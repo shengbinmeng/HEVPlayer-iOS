4ca36ce1a44a04682e98d52b1385b8201e1baf85
Shengbin Meng <shengbinmeng@gmail.com>
2015-08-27 17:07:42 +0800

    Add ffmpeg build instructions.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 HEVPlayer is a High Efficiency Video Player.
+
+## Build ffmpeg with lenthevcdec support
+
+1. Apply doc/*.patch to ffmpeg(v2.4.4) source (`patch -p1 < *.patch`).
+
+2. Run doc/config-ios.sh in ffmpeg source folder to configure, then `make` and `make install`.
+
+3. The header files and libraries are installed to the folder configured with "--prefix" and good to use.
+
+The folder hierarchy required by config-ios.sh is:
+
+	| ffmpeg source codes
+	| config-ios.sh
+	| thirdparty
+		| lenthevcdec
+			| include
+				| lenthevcdec.h
+			| lib
+				| liblenthevcdec.a
---
+++ doc/config-ios.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#
+# Change these variables according to your system.
+#
+SDK_VERSION=8.3
+IOS_ARCH=armv7
+TOOLCHAIN=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain
+SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${SDK_VERSION}.sdk
+PREFIX=./ios/${IOS_ARCH}
+LENTHEVCDEC=./thirdparty/lenthevcdec
+
+#
+# Read the configure help carefully before you want to change the following options.
+#
+./configure \
+	--prefix=${PREFIX} \
+	--target-os=darwin \
+	--arch=arm \
+	--cpu=armv7-a \
+	--enable-cross-compile \
+	--cross-prefix=${TOOLCHAIN}/usr/bin/ \
+	--cc=${TOOLCHAIN}/usr/bin/cc \
+	--sysroot=${SYSROOT} \
+	--extra-cflags="-O2 -I$LENTHEVCDEC/include -arch ${IOS_ARCH} -mfpu=neon -mfloat-abi=softfp" \
+	--extra-ldflags="-L$LENTHEVCDEC/lib -arch ${IOS_ARCH} -isysroot ${SYSROOT}" \
+	--enable-gpl \
+	--enable-version3 \
+	--enable-nonfree \
+	--disable-doc \
+	--disable-programs \
+	--enable-ffmpeg \
+	--disable-avdevice \
+	--disable-postproc \
+	--disable-devices \
+	--disable-filters \
+	--disable-bsfs \
+	--disable-zlib \
+	--disable-bzlib \
+	--disable-encoders \
+	--disable-muxers \
+	--enable-liblenthevcdec \
